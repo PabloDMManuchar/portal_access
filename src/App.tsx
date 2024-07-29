ba67875f096b260d70c22ced6585e306ea5c754e
PabloDMManuchar <pablo.demarco@manuchar.com>
2024-07-29 09:48:37 -0300

    Add Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import "./App.css";
-import CardButton from "./components/molecules/cardButton/cardButton";
+import Home from "./components/organisms/home/Home";
 
 function App() {
   return (
     <>
-      <div className="container-title">
-        <h1 className="grid text-center text-light p-0">Portal Acceso</h1>
-      </div>
+      <Home />
 
-      <CardButton />
-      
       {/* <footer className="footer">
         <img src={manucharLogo} alt="LogoIT" className="imagen-footer" />
         <img src={logoItMar} alt="LogoIT" className="imagen-footer" />
---
+++ src/components/organisms/home/Home.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import CardButton from "../../molecules/cardButton/cardButton";
+
+const Home: React.FC = () => {
+  const isAdmin = true;
+
+  return (
+    <>
+      <div className="my-6 w-full text-center flex  items-center justify-center gap-2 p-6 bg-gray-900 rounded-lg shadow-lg">
+        <h1 className="py-4 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-6xl font-bold text-transparent ">
+          PORTAL
+        </h1>
+        <h2 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-6xl text-transparent ">
+          ACCESS
+        </h2>
+      </div>
+      {isAdmin && <CardButton  group={'ventas'} />}
+    </>
+  );
+};
+
+export default Home;
